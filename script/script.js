6d0c397b30539ee6ee20b8eb09eefd611fb1968d
rtzon150 <wedenis866@gmail.com>
2025-01-22 21:37:47 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $('.carousele').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true, // Відображення крапок для навігації
+    arrows: true, // Відображення стрілок
+  });
+});
+
